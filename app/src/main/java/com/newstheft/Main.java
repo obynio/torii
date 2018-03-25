babe44cd9ac1bdc7ef5fcaa69dffac5e0df29263
Yohann Leon <yohann@leon.re>
2018-03-26 01:02:42 +0200

    git: initital commit

---
+++ app/src/main/java/com/newstheft/Main.java
@@ -0,0 +1,32 @@
+package com.newstheft;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class Main implements IXposedHookLoadPackage
+{
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable
+    {
+        if (!lpparam.packageName.equals("com.google.android.apps.magazines"))
+            return;
+
+        Class<?> edition = XposedHelpers.findClass("com.google.apps.dots.android.newsstand.edition.Edition",
+                lpparam.classLoader);
+
+        XposedHelpers.findAndHookMethod("com.google.apps.dots.android.newsstand.edition.LibrarySnapshot", lpparam.classLoader, "isPurchased", edition, new XC_MethodReplacement(){
+            @Override
+            protected Object replaceHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+            {
+                // Where the magic happens
+                return true;
+            }
+        });
+
+        XposedBridge.log("All your news are belong to us");
+    }
+}
